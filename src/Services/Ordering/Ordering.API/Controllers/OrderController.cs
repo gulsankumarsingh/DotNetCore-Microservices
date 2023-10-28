a4b217da964fd27b5f0453900ebe128f005da06c
Gulsan Kumar Singh <rathor.gulshan1@gmail.com>
2023-10-28 23:52:04 +0530

    Ordering API setup

--- src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,11 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Exceptions;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrderList;
 
 namespace Ordering.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet("{userName}", Name = "GetOrder")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<OrdersDto>))]
+        public async Task<IActionResult> GetOrderByUserName(string userName)
+        {
+            var query = new GetOrderListQuery(userName);
+            var orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+        [HttpPost(Name = "CheckoutOrder")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> CheckoutOrder([FromBody]CheckoutOrderCommand command)
+        { 
+            var orders = await _mediator.Send(command);
+            return Ok(orders);
+        }
+
+        [HttpPut(Name = "UpdateOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(NotFoundExpection))]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPut(Name = "DeleteOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundExpection))]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> DeleteOrder([FromBody] DeleteOrderCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
     }
 }
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,3 +1,8 @@
+using Ordering.API.Extensions;
+using Ordering.Application.StartupExtensions;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.StatupExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +12,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 var app = builder.Build();
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
