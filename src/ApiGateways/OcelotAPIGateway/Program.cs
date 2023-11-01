9ba3d953cdfee013874eb39085ff1ad7caacccdb
Gulsan Kumar Singh <rathor.gulshan1@gmail.com>
2023-11-01 23:55:38 +0530

    Added  OcelotAPiGateway

---
+++ src/ApiGateways/OcelotAPIGateway/Program.cs
@@ -0,0 +1,31 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configure Logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+//Configure App Configuration
+builder.Configuration.AddJsonFile($"ocelotsettings.{builder.Environment.EnvironmentName}.json", true, true);
+
+//Configure Ocelot
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
